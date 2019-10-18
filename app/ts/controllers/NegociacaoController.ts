054be86ca599a42d7c8cace3582b5fd1fdf223ac
tiagoluzh607 <tiagoluz.h607@gmail.com>
2019-10-18 18:49:59 -0300

    buscando dados via get e usando interface

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -1,4 +1,4 @@
-import { Negociacoes, Negociacao } from "../models/index";
+import { Negociacoes, Negociacao, NegociacaoParcial } from "../models/index";
 import { NegociacoesView, MensagemView } from "../views/index";
 import { domInject } from "../helpers/decorator/index";
 
@@ -48,6 +48,29 @@ export class NegociacaoController{
 
     private _ehDiaUtil(data: Date): boolean{
         return data.getDay() != DiaDaSemana.Sabado || data.getDay() != DiaDaSemana.Domingo;
+    }
+
+    importaDados(){
+        
+        function isOk(res: Response){
+            if(res.ok){
+                return res;
+            }else{
+                throw new Error(res.statusText);
+            }
+        }
+
+        fetch('http://localhost:8080/dados')
+            .then(res => isOk(res))
+            .then(res => res.json())
+            .then((dados: NegociacaoParcial[]) => {
+                dados
+                    .map(dado => new Negociacao(new Date(), dado.vezes, dado.montante))
+                    .forEach(negociacao => this._negociacoes.adiciona(negociacao))
+
+                this._negociacoesView.update(this._negociacoes)
+            })
+            .catch(err => console.log(err.message));
     }
 }
 
