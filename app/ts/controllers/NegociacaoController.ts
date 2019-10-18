6937074a52f4f98fee284ee833b00958c660516a
tiagoluzh607 <tiagoluz.h607@gmail.com>
2019-10-18 13:21:43 -0300

    Enums em typescript

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -20,8 +20,15 @@ export class NegociacaoController{
 
         event.preventDefault();
 
+        let data = new Date(this._inputData.val().replace(/-/g, ','));
+
+        if(!this._ehDiaUtil(data)){
+            this._mensagemView.update('Somente negociações em dias úteis, por favor!');
+            return;
+        }
+
         const negociacao = new Negociacao(
-            new Date(this._inputData.val().replace(/-/g, ',')),
+            data,
             parseInt(this._inputQuantidade.val()),
             parseFloat(this._inputValor.val())
         );
@@ -38,4 +45,18 @@ export class NegociacaoController{
 
         this._mensagemView.update('Negociação adicionada com sucesso');
     }
+
+    private _ehDiaUtil(data: Date): boolean{
+        return data.getDay() != DiaDaSemana.Sabado || data.getDay() != DiaDaSemana.Domingo;
+    }
+}
+
+enum DiaDaSemana{
+    Domingo,
+    Segunda,
+    Terca,
+    Quarta,
+    Quinta,
+    Sexta,
+    Sabado
 }
